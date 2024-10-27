5967019a4d36ff476e3b67b2aa15d85e3840eb08
denys767 <k.denislife@gmail.com>
2024-10-27 12:42:26 +0200

    Initial commit

---
+++ fEr_DZ_11.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+#pragma warning (disable:4996)
+
+//#include "c:\\settings\\set.set"
+
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int* fEr2(int R) {
+	int* res, * ps, * pt, * pfin, * pv, v, ci;
+	for (*(res = new int[R])=2,ci = R - 1, v = 2, pfin = res; ci--; *++pfin = ++v);
+	for (ps = pv = res; 1; ++pv) {
+		while (*pv == 0 && pv != pfin)
+			++pv;// pv - param for '2 cycles'
+		if (pv == pfin) return (int*)realloc(res, (ps - res)*sizeof(*res)) ;
+		*ps++ = *pv;
+		for (v = *pv, pt = pv; pt < pfin;*pt = 0, pt += v);
+	}	
+}
+
+int* fEr(int& R) {
+	int* res, * ps, * pt, * pfin, * pv, v, ci;
+	res = new int[R];
+	*res = 2;
+	for (ci = R - 1, v = 2, pfin = res; ci--; ) {
+		*++pfin = ++v;
+	}
+	for (ps = pv = res; 1; ++pv) {
+		while (*pv == 0 && pv != pfin)
+			++pv;
+		if (pv == pfin)break;
+		*ps = *pv; ++ps;
+		for (v = *pv, pt = pv; pt < pfin; pt += v)
+			*pt = 0;
+	}
+	R = ps - res;
+	return res;
+}
+int main() {
+	// Write C++ code here
+	int* p, r = 100;
+	p = fEr(r);
+	for (int ci = r; ci--; ++p) std::cout << *p << ' ';
+	delete[](p-r);
+	std::cout << "\n";
+
+	p = fEr2(100);
+	for (int ci = _msize(p)/sizeof(*p), *pc=p; ci--;std::cout << *pc++ << ' ') ;
+	delete[]p;
+	std::cout << "\nTry programiz.pro";
+
+	return 0;
+}
---
+++ fSh_DZ_12.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+#pragma warning (disable:4996)
+
+//#include "c:\\settings\\set.set"
+
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+void fs(int* p, int qr, int qc, int sh) {
+	int* psl = new int[sh];
+	int qu = qr * qc, ci;
+	int* ps, * pt;
+	for (pt = psl, ps = p + qu - sh, ci = sh; ci-- /* !=0*/; ++pt, ++ps) {
+		*pt = *ps; std::cout << " " << *pt;
+	}std::cout << std::endl;
+	for (pt = p + qu - 1, ps = p + qu - 1 - sh, ci = qu - sh; ci--; --pt, --ps)
+	{
+		*pt = *ps; std::cout << " " << *pt;
+	}std::cout << std::endl;
+	for (pt = p, ps = psl, ci = sh; ci--; +pt, ++ps)
+	{
+		*pt = *ps; std::cout << " " << *pt;
+	}std::cout << std::endl;
+
+	delete[]psl;
+}
+void fsStandart(int* p, int qrqc, int sh) {
+	int* psl;
+	memcpy(psl = new int[sh], p+qrqc-sh, sh*sizeof(*p));
+	memcpy(p+sh, p, (qrqc-sh) * sizeof(*p));
+	memcpy(p, psl, sh * sizeof(*p));
+	delete[]psl;
+}
+int main() {
+
+	int m2[2][4] = { {11,17,13,15},
+	{23,27,21,29} };
+	fs((int*)m2, 2, 4, 3);
+	for (int j = 0; j < 4 * 2; ++j) std::cout << ' ' << m2[0][j];
+	fsStandart((int*)m2, 2* 4, 3);
+	std::cout << "\n\n";
+	for (int j = 0; j < 4 * 2; ++j) std::cout << ' ' << m2[0][j];
+	// Write C++ code here
+	std::cout << "\nTry programiz.pro";
+
+	return 0;
+}
