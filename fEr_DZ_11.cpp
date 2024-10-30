917716f562c58ad59a163c8be9ac1d29127f7624
denys767 <k.denislife@gmail.com>
2024-10-30 22:56:48 +0200

    changes

--- fEr_DZ_11.cpp
+++ fEr_DZ_11.cpp
@@ -21,6 +21,33 @@ int* fEr2(int R) {
 	}	
 }
 
+/*
+int* fEr2(int R) {
+    int* res = new int[R];
+    int* ps = res;
+    int* pfin = res + R;
+    int v = 2;
+
+    for (int* p = res; p < pfin; ++p) {
+        *p = v++;
+    }
+
+    for (int* pv = res; pv < pfin; ++pv) {
+        if (*pv == 0) continue;
+        *ps++ = *pv;
+        for (int* pt = pv + *pv; pt < pfin; pt += *pv) {
+            *pt = 0;
+        }
+    }
+
+    int newSize = ps - res;
+    int* result = new int[newSize];
+    std::copy(res, ps, result);
+    delete[] res;
+    return result;
+}
+*/
+
 int* fEr(int& R) {
 	int* res, * ps, * pt, * pfin, * pv, v, ci;
 	res = new int[R];
@@ -54,3 +81,60 @@ int main() {
 
 	return 0;
 }
+
+/*
+#include <iostream>
+#include <fstream>
+using namespace std;
+#pragma warning (disable:4996)
+
+int* fEr(int& R) {
+	int* res = new int[R];
+	int* ps = res;
+	int* pfin = res + R;
+	int v = 2;
+
+	*res = 2;
+	for (int ci = R - 1; ci--; ) {
+		*++ps = ++v;
+	}
+	ps = res;
+	for (int* pv = res; pv < pfin; ++pv) {
+		while (*pv == 0 && pv != pfin)
+			++pv;
+		if (pv == pfin) break;
+		*ps++ = *pv;
+		for (int* pt = pv + *pv; pt < pfin; pt += *pv)
+			*pt = 0;
+	}
+	R = ps - res;
+	return res;
+}
+
+int* fEr2(int R) {
+	int* res, * ps, * pt, * pfin, * pv, v, ci;
+	for (*(res = new int[R]) = 2, ci = R - 1, v = 2, pfin = res; ci--; *++pfin = ++v);
+	for (ps = pv = res; 1; ++pv) {
+		while (*pv == 0 && pv != pfin)
+			++pv;
+		if (pv == pfin) return (int*)realloc(res, (ps - res) * sizeof(*res));
+		*ps++ = *pv;
+		for (v = *pv, pt = pv; pt < pfin; *pt = 0, pt += v);
+	}
+}
+
+int main() {
+	int* p, r = 100;
+	p = fEr(r);
+	for (int ci = r; ci--; ++p) std::cout << *p << ' ';
+	delete[](p - r);
+	std::cout << "\n";
+
+	p = fEr2(100);
+	for (int ci = 100, *pc = p; ci--; std::cout << *pc++ << ' ');
+	delete[]p;
+	std::cout << "\nTry programiz.pro";
+
+	return 0;
+}
+*/
